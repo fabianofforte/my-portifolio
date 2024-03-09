cb49658b274fdf3c1bca7e9d6d230becf9c7a93b
Fabiano Ferreira <fabiano.fforte@gmail.com>
2024-03-09 20:27:25 -0300

    feat: add html structure

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import "./header.scss";
+
+interface HeaderProps{
+
+}
+
+export function Header(){
+    return(
+        <div className="header">
+          <div>
+            <h1>Olá, meu nome é Fabiano!👋 </h1>
+            <h2>Desenvolvedor Full Stack</h2>
+          </div>
+            <Image
+              src="/me.jpeg"
+              alt="Vercel Logo"
+              width={325}
+              height={310}
+              priority
+            />
+        
+      </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,39 @@
-import Image from "next/image";
+import { Header } from "./components/header/header";
+import "./styles/home.scss";
 
 export default function Home() {
   return (
-    <main>
-      <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              width={100}
-              height={24}
-              priority
-      />
+    <main className="container">
+      <Header />
+      <div className="experience">
+        <h3>Experiencia</h3>
+        <p>Trabalhei como desenvolvedor FullStack em pequenos projetos freelance por 2 anos.</p>
+        <div className="experience-time">
+
+        </div>
+        <div className="infos">
+          <h3>Idioma</h3>
+          <div className="languages-info">
+            <span>🇺🇸 EN - Estudando</span>
+            <span>🇧🇷 PT-BR - Nativo</span>
+          </div>
+          <h3>Formação</h3>
+          <div className="educacional-info">
+            <span>🎓</span>
+            <span>Engenharia de Software - Faculdade Anhanguera (1º Semestre)</span><br />
+            <span>🎓</span>
+            <span>Tecnico Eletrônica - Escola Parobé</span>
+          </div>
+        </div>
+
+        <div className="buttons">
+          <div className="social">
+
+          </div>
+          <button>contact me</button>
+        </div>
+      </div>
+      
     </main>
   );
 }
